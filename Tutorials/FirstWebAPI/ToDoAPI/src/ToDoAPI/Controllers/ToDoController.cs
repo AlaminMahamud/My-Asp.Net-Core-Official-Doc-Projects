1d9b3c3e5d0b21b7f1f72c044ff7162ced224b9c
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-07-24 20:34:50 +0600

    First Web API

---
+++ Tutorials/FirstWebAPI/ToDoAPI/src/ToDoAPI/Controllers/ToDoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        public IToDoRepository ToDoItems { get; set; }
+        public ToDoController(IToDoRepository todoItems)
+        {
+            ToDoItems = todoItems;
+        }
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return ToDoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = ToDoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            ToDoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new {id= item.Key}, item);
+        }
+    }
+}
---
+++ Tutorials/FirstWebAPI/ToDoAPI/src/ToDoAPI/Models/ToDoRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAPI.Models
+{
+    public class ToDoRepository : IToDoRepository
+    {
+        private static ConcurrentDictionary<string, ToDoItem> _todos = new ConcurrentDictionary<string, ToDoItem>();
+        public ToDoRepository()
+        {
+            Add(new ToDoItem { Name = "Item - 1" });
+        }
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return _todos.Values;
+        }
+        public void Add(ToDoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _todos[item.Key] = item;  
+        }
+
+        public ToDoItem Find(string key)
+        {
+            ToDoItem item;
+            _todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public ToDoItem Remove(string key)
+        {
+            ToDoItem item;
+            _todos.TryGetValue(key, out item);
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(ToDoItem item)
+        {
+            _todos[item.Key] = item;
+        }
+    }
+}
